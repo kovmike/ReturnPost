08cacf626dbf3a803de475497dcc866f281112b6
kovmike <>
2020-06-29 20:18:16 +0300

    1
    1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+// import { using, spec, list, h } from "forest";
+import "./App.css";
 
-function App() {
+export const App = () => {
+  const inpud = useRef(null);
+  const labela = useRef(null);
+
+  const click = () => {
+    const data = inpud.current.value ? { payload: `${inpud.current.value}` } : { payload: "17095247999439" };
+
+    fetch("http://127.0.0.1:8000/", { method: "POST", body: JSON.stringify(data) })
+      .then((r) => r.json())
+      .then((data) => (labela.current.innerHTML = JSON.stringify(data)));
+  };
+
+  const clickDictionary = () => {
+    fetch("https://tariff.pochta.ru/tariff//v1/dictionary?jsontext&category=all")
+      .then((r) => r.json())
+      .then((data) => (labela.current.innerHTML = JSON.stringify(data)));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" ref={inpud}></input>
+      <button onClick={click}>click</button>
+      <button onClick={clickDictionary}>click</button>
+      <label ref={labela}>2314</label>
     </div>
   );
-}
-
-export default App;
+};
